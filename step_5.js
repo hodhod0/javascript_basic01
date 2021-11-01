d50a0d50f2ad7884f35433700469c9884f276870
hodhod0 <alameddinehodhod@gmail.com>
2021-11-01 20:18:39 +0200

    step 5

---
+++ step_5.js
@@ -0,0 +1,10 @@
+var first = document.getElementById("fn");
+var sec = document.getElementById("sn");
+var btn = document.getElementById("submit");
+var result = document.getElementById("result");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  var total = first.value * sec.value;
+  result.innerText = total;
+});
